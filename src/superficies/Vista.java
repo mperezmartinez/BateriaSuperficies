f5408340f7eddc731bea099a25489f8758bc9661
Miguel <Miguel@Mipema>
2015-05-07 09:47:02 +0200

    Creada la clase vista que tine salida de datos por getters

---
+++ src/superficies/Vista.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package superficies;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Vista {
+    
+    
+        public static void imprimir(Modelo obj){
+        
+  if (obj.getFigura().compareToIgnoreCase("Cuadrado") == 0){
+     JOptionPane.showMessageDialog(null, "Cuadrado "+obj.getBase()+"*"+obj.getAltura()+ "= "+obj.getArea());    
+  } 
+   if (obj.getFigura().compareToIgnoreCase("Rectangulo") == 0){
+     JOptionPane.showMessageDialog(null, "Rectangulo "+obj.getBase()+"*"+obj.getAltura()+ "= "+obj.getArea());    
+  }
+    if (obj.getFigura().compareToIgnoreCase("Triangulo") == 0){
+     JOptionPane.showMessageDialog(null, "Triangilo "+obj.getBase()+"*"+obj.getAltura()+"/2"+  "= "+obj.getArea());    
+  }
+     if (obj.getFigura().compareToIgnoreCase("Circulo") == 0){
+   JOptionPane.showMessageDialog(null, "Circulo "+obj.getRadio()+"*"+obj.getRadio()+"*"+"3.14159265" +"= "+obj.getArea()); 
+  }
+    }
+    
+    
+    
+}
