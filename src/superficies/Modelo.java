a4aa04cf3840150c61cd0155621b6a43bca80908
Miguel <Miguel@Mipema>
2015-05-07 08:54:16 +0200

    Creada la clase modelo con los atributos correspondientes y sus getters y setters

---
+++ src/superficies/Modelo.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package superficies;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Modelo {
+    
+    private float base;
+    private float altura;
+    private float radio;
+    private float area;
+    private String figura;
+
+    public float getBase() {
+        return base;
+    }
+
+    public void setBase(float base) {
+        this.base = base;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getRadio() {
+        return radio;
+    }
+
+    public void setRadio(float radio) {
+        this.radio = radio;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+
+    public String getFigura() {
+        return figura;
+    }
+
+    public void setFigura(String figura) {
+        this.figura = figura;
+    }
+    
+    
+    
+    
+}
