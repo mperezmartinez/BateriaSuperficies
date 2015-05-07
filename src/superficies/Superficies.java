1ae989b1ae42e6ab0b5d96d5465e7275ac493da0
Miguel <Miguel@Mipema>
2015-05-07 09:29:44 +0200

    Creado un menu para elejir el tipo de operacion que queremos hacer

--- src/superficies/Superficies.java
+++ src/superficies/Superficies.java
@@ -1,6 +1,7 @@
 package superficies;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  * Examen de MVC 3ª Avaliación
@@ -15,80 +16,48 @@ public class Superficies {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String shapeType;
+        
+        Modelo contenedor = new Modelo();
+        
+  int  opc = JOptionPane.showOptionDialog(
+                null, "MENU", "ELIJA SU OPERACION", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, new Object[]{
+                    "Cuadrado",
+                    "Rectangulo",
+                    "Triangulo",
+                    "Circulo"},
+                "Exit")+1;
 
-        sc = new Scanner(System.in);
-
-        System.out.println("What shape would you like to find the area of? (square, circle, triangle, rectangle):");
-        shapeType = sc.nextLine();
-        System.out.println("You said: " + shapeType);
-
-        if (shapeType.equals("square")) {
-            //add area calculations for square
-            float squareArea;
-            float sideLength;
-
-            System.out.println("what is the side length?");
-            sideLength = sc.nextFloat();
-            squareArea = sideLength * sideLength;
-            System.out.println("The area for your square: " + squareArea);
-
-        }
-        if (shapeType.equals("rectangle")) {
-            //add area calculations for rectangle here
-            float sideLength;
-            float sideHeight;
-            float rectangleArea;
-
-            System.out.println("what is the rectangles width?");
-            sideLength = sc.nextFloat();
-            System.out.println("What is the rectangles height?");
-            sideHeight = sc.nextFloat();
-            rectangleArea = sideLength * sideHeight;
-            System.out.println("The area for your rectangle is: " + rectangleArea);
-
-        }
-        if (shapeType.equals("triangle")) {
-            //add area calculations for triangle here
-            float baseLength;
-            float height;
-            float triangleArea;
-
-            System.out.println("What is the base length of the triangle?");
-            baseLength = sc.nextFloat();
-            System.out.println("What is the height of the triangle?");
-            height = sc.nextFloat();
-            triangleArea = (float) (0.5 * baseLength * height);
-            System.out.println("Your triangles area is: " + triangleArea);
-
-        }
-        if (shapeType.equals("circle")) {
-            //add area calculations for a circle here
-            float radius;
-            float circleArea;
-
-            System.out.println("What is the radius of the circle?");
-            radius = sc.nextFloat();
-            circleArea = radius * radius;
-            circleArea = (float) (3.14159265 * circleArea);
-            System.out.println("Your Circles area is " + circleArea);
-
+        switch (opc) {
+            case 1:
+                contenedor.setFigura("Cuadrado");
+                break;
+            case 2:
+                contenedor.setFigura("Rectangulo");
+                break;
+            case 3:
+                contenedor.setFigura("Triangulo");
+                break;
+            case 4:
+                contenedor.setFigura("Circulo");
+                break;
+            default:
+                System.exit(0);
         }
 
     }
 
-    public static float realizaOperacion(float base, float altura, float radio, int oper) {
+    public static float realizaOperacion(float base, float altura, float radio, String oper) {
         float res = 0;
-        if (oper == 1) {
+        if (oper.compareToIgnoreCase("Cuadrado") == 0) {
             res = base * altura;
         }
-        if (oper == 2) {
+        if (oper.compareToIgnoreCase("Rectangulo") == 0) {
             res = base * altura;
         }
-        if (oper == 3) {
+        if (oper.compareToIgnoreCase("Triangulo") == 0) {
             res = base * altura / 2;
         }
-        if (oper == 4) {
+        if (oper.compareToIgnoreCase("Circulo") == 0) {
             res = (float)(radio * radio * 3.14159265);
         }
         return res;
